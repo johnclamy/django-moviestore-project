5672025d74f8118dd174a6f187d212b8a2e86439
johnclamy <johnclamy@gmail.com>
2025-02-10 19:26:19 +0000

    create a template_data dictionary in the index function to passed down data to the  home template file

--- moviestore/home/views.py
+++ moviestore/home/views.py
@@ -2,7 +2,10 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'home/index.html')
+    template_data = {}
+    template_data['title'] = 'M Store: Buy you DVDs with us!'
+
+    return render(request, 'home/index.html', {'template_data': template_data})
 
 
 def about(request):
