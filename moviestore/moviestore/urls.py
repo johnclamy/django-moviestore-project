ad1bb18aeb902ced43dce2f6ef4750e4f50a1aad
johnclamy <johnclamy@gmail.com>
2025-02-16 22:48:32 +0000

    add the accounts url file in the project-level url file

--- moviestore/moviestore/urls.py
+++ moviestore/moviestore/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
     path('movies/', include('movies.urls')),
+    path('accounts/', include('accounts.urls')),
 ]
 
 
