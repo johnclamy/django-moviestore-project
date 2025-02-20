439df5ce6b007b16b82fd0f150bb921393056e44
johnclamy <johnclamy@gmail.com>
2025-02-20 17:08:51 +0000

    set the signup function in '/accounts/views.py' file to use the cutom error list on the sign up  form

--- moviestore/accounts/views.py
+++ moviestore/accounts/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.shortcuts import redirect
-from .forms import CustomUserCreationForm
+from .forms import CustomUserCreationForm, CustomErrorList
 
 
 def signup(request):
@@ -11,7 +11,7 @@ def signup(request):
         template_data['form'] = CustomUserCreationForm()
         return render(request, 'accounts/signup.html', {'template_data': template_data})
     elif request.method == 'POST':
-        form = CustomUserCreationForm(request.POST)
+        form = CustomUserCreationForm(request.POST, error_class=CustomErrorList)
         if form.is_valid():
             form.save()
             return redirect('home.index')
